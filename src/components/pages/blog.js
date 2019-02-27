c385bbd7dc987a240746c7e7462e93bd6ce7fb09
Derek Gilbert <derek@thedevelopmentphase.com>
2019-02-27 12:14:18 -0700

    Update blog.js

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
-export default function(){
-  return (
-    <div>
-      <h2>Blog</h2>
+class Blog extends Component {
+  constructor() {
+    super();
+  }
+  render () {
+    return (
       <div>
-        <Link to="about">Read More About Me</Link>
+        <h2>Blog</h2>
+        <div>
+          <Link to="about">Read More About Me</Link>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
